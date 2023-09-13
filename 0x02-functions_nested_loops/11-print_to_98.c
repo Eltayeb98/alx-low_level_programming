f47f06cb0dea6ab8675477ce9af61b975fb63177
Eltayeb98 <atayibaltijani@gmail.com>
2023-09-13 14:43:47 +0200

    102-fibonacci.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,42 +1,32 @@
+#include <stdio.h>
 #include "main.h"
-#include 
+
 /**
- * print_to_98 - print a program to 98
- * @n: The number to start printing
- * Return: Always 0.
+ * print_to_98 - prints all natural numbers from n to 98,
+ * followed by a new line
+ * @n: print from this number
  */
-void print_to_98(int n;)
+void print_to_98(int n)
 {
+	int i, j;
+
 	if (n <= 98)
 	{
-	for (; n <= 98; n++)
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
 	{
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
-	}
-	else
-	{
-	printf("%d, ", n);
-	}
-	}
-	}
-	else
-	{
-	for (; n >= 98; n--)
-	{
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
-	}
-	else
-	{
-		printf("%d, ", n);
-	}
-	}
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
 	}
 }
