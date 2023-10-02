89692c32a4e17683d3d9a422cbcc375942a5ac75
Eltayeb98 <atayibaltijani@gmail.com>
2023-10-02 22:13:26 +0200

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,8 +3,8 @@
 
 /**
  * main - print the number of arguments passed to the program
- * argc: number of arguments
- * argv: array of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
